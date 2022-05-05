7400c0a60c1d76e280e78ce8f2d06352c9fd1b50
Alexander Garro Diaz <agarrodiaz@hotmail.com>
2022-05-04 21:43:26 -0600

    ExamenFinal

---
+++ WebApiRest/App_Start/ContainerExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BD;
+using WBL;
+namespace WebApiRest
+{
+    public static class ContainerExtensions
+    {
+
+        public static IServiceCollection AddDIContainer(this IServiceCollection services)
+        {
+
+            services.AddSingleton<IDataAccess,DataAccess>();
+            services.AddTransient<IAgenciaService, AgenciaService>();
+            services.AddTransient<IAlquilerService, AlquilerService>();
+            services.AddTransient<IClientesService, ClientesService>();
+            services.AddTransient<ICatalogoCantonService, CatalogoCantonService>();
+            services.AddTransient<ICatalogoDistritoService, CatalogoDistritoService>();
+            services.AddTransient<ICatalogoProvinciaService, CatalogoProvinciaService>();
+            services.AddTransient<IMarcaVehiculoService, MarcaVehiculoService>();
+            services.AddTransient<IUsuariosServices, UsuariosServices>();
+            services.AddTransient<IVehiculoService, VehiculoService>();
+            return services;
+        }
+    }
+}
---
+++ WebAppLab/App_Start/ContainerExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BD;
+using WBL;
+namespace WebAppLab
+{
+    public static class ContainerExtensions
+    {
+
+        public static IServiceCollection AddDIContainer(this IServiceCollection services)
+        {
+
+            services.AddSingleton<IDataAccess,DataAccess>();
+
+            services.AddTransient<IMarcaVehiculoService, MarcaVehiculoService>();
+            services.AddTransient<IVehiculoService, VehiculoService>();
+            services.AddTransient<IAlquilerService, AlquilerService>();
+            services.AddTransient<IAgenciaService, AgenciaService>();
+            services.AddTransient<IClientesService, ClientesService>();
+            services.AddTransient<ICatalogoProvinciaService, CatalogoProvinciaService>();
+            services.AddTransient<ICatalogoCantonService, CatalogoCantonService>();
+            services.AddTransient<ICatalogoDistritoService, CatalogoDistritoService>();
+            services.AddTransient<IUsuariosServices, UsuariosServices>();
+
+            return services;
+        }
+    }
+}
